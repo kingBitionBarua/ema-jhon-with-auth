da43027dacaac80b7de7f923837bbf7b70b1c028
kingBitionBarua <kingbitionbarua7@gmail.com>
2023-04-24 23:56:11 +0600

    Implement Sign in and Sign out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+        })
+        .catch(error => {
+            console.log(error);
+            setError(error.message);
+        })
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
-            <form>
+            <form onSubmit={handleLogin}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name='email' required />
@@ -16,6 +40,9 @@ const Login = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p>
+                <small>New to Ema-john? <Link to="/signup">Create New Account</Link></small>
+            </p>
         </div>
     );
 };
